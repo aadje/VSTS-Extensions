bd226746a4572a5699e7621f3ee397b15b229c3c
Arjan van Rijn <Arjan van Rijn>
2018-01-07 20:41:37 +0100

    Added kuduzipdeploy Zip api support in 2.0.0

--- Tasks/KuduZipDeploy/buildtask/New-KuduZipDeploy.ps1
+++ Tasks/KuduZipDeploy/buildtask/New-KuduZipDeploy.ps1
@@ -8,7 +8,9 @@ $ManageSlotFlag = Get-VstsInput -Name ManageSlotFlag -AsBool
 $ResourceGroupName = Get-VstsInput -Name ResourceGroupName
 $SlotName = Get-VstsInput -Name SlotName
 $InputFilePath = Get-VstsInput -Name InputFilePath
+$ZipApi = Get-VstsInput -Name ZipApi
 $IsAsync = Get-VstsInput -Name IsAsync -AsBool
+$OutputFolderPath = Get-VstsInput -Name OutputFolderPath
 
 Write-Host "ConnectedServiceName $ConnectedServiceName"
 Write-Host "WebAppName $WebAppName"
@@ -16,7 +18,9 @@ Write-Host "ManageSlotFlag $ManageSlotFlag"
 Write-Host "ResourceGroupName $ResourceGroupName"
 Write-Host "SlotName $SlotName"
 Write-Host "InputFilePath $InputFilePath"
+Write-Host "ZipApi $ZipApi"
 Write-Host "IsAsync $IsAsync"
+Write-Host "OutputFolderPath $OutputFolderPath"
 
 if($ManageSlotFlag -and -not $SlotName){
     throw "$SlotName parameter needed for $Task"
@@ -43,7 +47,7 @@ if($apps.Count -ne 1){
 $app = $apps | Select-Object -First 1
 
 if($ManageSlotFlag){
-    $requestUri = "https://$WebAppName`-$SlotName.scm.azurewebsites.net/api/zipdeploy"
+    $requestUri = "https://$WebAppName`-$SlotName.scm.azurewebsites.net/api/"
     $resourceType = "Microsoft.Web/sites/slots/config"
     $resourceName = "$WebAppName/$SlotName/publishingcredentials"
 } else{
@@ -59,25 +63,44 @@ $header = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($header))
 $header = "Basic $header"
 $headers = @{"Authorization"=$header;"If-Match"="*"}
 
-if($IsAsync){
-    $requestUri = $requestUri + "?isAsync=true"
+if($ZipApi -eq 'ZipDeploy'){
+    $requestUri = $requestUri + "zipdeploy"
+    if($IsAsync){
+        $requestUri = $requestUri + "?isAsync=true"
+    }
+    
+    write-host "Uploading to $requestUri ..."
+    Write-Host "Deployment logs at https://$WebAppName.scm.azurewebsites.net/api/deployments"
+    
+    $response = Invoke-WebRequest `
+        -Uri $requestUri `
+        -Headers $headers `
+        -Method Post `
+        -InFile $InputFilePath `
+        -ContentType "multipart/form-data" `
+        -ErrorAction SilentlyContinue `
+        -UseBasicParsing
+
+} elseif (Zip -eq 'Zip') {
+    $requestUri = $requestUri + "zip/"
+    if($OutputFolderPath){
+        $requestUri = $requestUri + $OutputFolderPath
+    }
+
+    Write-Host "Uploading zip to $requestUri"
+
+    $response = Invoke-WebRequest `
+        -Uri $requestUri `
+        -Headers $headers `
+        -Method Put `
+        -InFile $InputFilePath `
+        -ContentType "multipart/form-data" `
+        -ErrorAction SilentlyContinue `
+        -UseBasicParsing
 }
-
-write-host "Uploading to $requestUri ..."
-
-$response = Invoke-WebRequest `
-    -Uri $requestUri `
-    -Headers $headers `
-    -Method Post `
-    -InFile $InputFilePath `
-    -ContentType "multipart/form-data" `
-    -ErrorAction SilentlyContinue `
-    -UseBasicParsing
 
 Write-Host "Response: $($response.StatusCode) $($response.StatusDescription) `n$($response.Content)"
 
-if(-not $IsAsync){
-    Write-Host "Deployment logs at https://$WebAppName.scm.azurewebsites.net/api/deployments"
-} elseif ($response.StatusCode -eq 202) {
+if($response.StatusCode -eq 202) {
     Write-Host "Deployment is running at location $($response.Headers.Location)"
 }
